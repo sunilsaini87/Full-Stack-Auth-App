45a9ebdc6cd41a4319f13517cb77aeb340bb1533
Sunil Kumar Saini <sunilkumarsaini6363@gmail.com>
2024-04-08 18:12:48 +0530

    update files

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <nav>
+      <Link to='/'>Home</Link>
+      <Link to='/register'>Register</Link>
+      <Link to='/login'>Login</Link>
+    </nav>
   )
 }
 
-export default Navbar
+export default Navbar;
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,7 +1,27 @@
-import React from "react";
+import { useState } from "react";
 
 function Login() {
-  return <div>Login</div>;
+  const [data, setData] = useState({
+    email: '',
+    password: '',
+  })
+
+
+
+  const loginUser = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <div>
+      <form onSubmit={loginUser}>
+        <label>Email</label>
+        <input type="email" placeholder="Enter Your Email" value={data.email} onChange={(e) => setData({...data, email: e.target.value})} />
+        <label>Password</label>
+        <input type="password" placeholder="Enter Your Password" value={data.password} onChange={(e) => setData({...data, password: e.target.value})} />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
 }
 
 export default Login;
