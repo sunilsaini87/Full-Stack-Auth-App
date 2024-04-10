9e8b5a822e8c10303cc7abfcbe721bcdab2b3300
Sunil Kumar Saini <sunilkumarsaini6363@gmail.com>
2024-04-10 19:11:57 +0530

    update files

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,7 @@
-// import { useState } from "react";
-// import reactLogo from "./assets/react.svg";
-import "./App.css";
+import React from "react";
 
 function App() {
-  // const [count, setCount] = useState(0);
-
-  return <></>;
+  return <div>App</div>;
 }
 
 export default App;
